765405bc48dbd274046901df5af5a8d1ff3ce0a1
Janani-Jayachandran <jananijayachandran27@gmail.com>
2023-11-28 12:34:57 +0530

    added active functions

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ $(".menu > ul > li").click(function (e){
 $(".menu-btn").click(function(){
     $(".sidebar").toggleClass("active");
 });
+
+const links= document.querySelectorAll(".nav > div > ul > li");
+const cards= document.querySelectorAll(".card");
+
+[...links].map((link, index) => {
+    link.addEventListener("click",() => onLinkClick(link, index), false);
+});
+
+const onLinkClick = (link, currentIndex) => {
+    const selectedItem = link.getAttribute("name");
+    cards.forEach((card) => {
+        card.classList.remove("active");
+    });
+    const currentCard =[...cards].find((card) =>
+        card.classList.contains(selectedItem)
+    );
+    currentCard.classList.add("active");
+};
