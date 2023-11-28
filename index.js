98871e36ea78f5c946def4925a494a07a4493586
Gowri K <gowrikalimuthu2003@gmail.com>
2023-11-28 19:04:19 +0530

    eventfunc added

--- index.js
+++ index.js
@@ -28,3 +28,42 @@ const onLinkClick = (link, currentIndex) => {
     );
     currentCard.classList.add("active");
 };
+
+var form = document.getElementById("#myForm"),
+  imgInput = document.querySelector(".img"),
+  file = document.getElementById("imgInput"),
+  name = document.getElementById("name"),
+  email = document.getElementById("email"),
+  phone = document.getElementById("phone"),
+  sDate = document.getElementById("sDate"),
+  submitBtn = document.querySelector(".submit"),
+  userInfo = document.getElementById("data")
+
+
+
+  let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : []
+
+  let isData = false, editId
+
+  file.onchange = function(){
+    if(file.files[0].size < 1000000){
+        var fileReader = new FileReader();
+
+        fileReader.onload = function(e){
+            imgUrl = e.target.result
+            imgInput.src = imgUrl
+        }
+        fileReader.readAsDataURL(file.files[0])
+    }
+    else {
+        alert("This file is too large !")
+    }
+  }
+
+
+  form.addEventListener('submit' , (e)=>{
+    e.preventDefault()
+    const information = {
+        picture:  imgInput.src == undefined ? "Images/user-1.jpg" : imgInput.src,
+    }
+  })
