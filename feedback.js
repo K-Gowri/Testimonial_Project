cd5d3f9d42e86ee261a7ed1bed7fab7f7a20b254
Pranav Krishnaa S <pranavkrishnaa.2003@gmail.com>
2024-04-05 12:33:03 +0530

    Added json file

---
+++ feedback.js
@@ -0,0 +1,64 @@
+let form = document.querySelector('form');
+const API_URL = 'http://localhost:3000/feedback'
+const userName = document.getElementById('name')
+const email = document.getElementById('email')
+const join = document.getElementById('join')
+const travel = document.getElementById('travel')
+const words = document.getElementById('words')
+const most = document.getElementById('most')
+const burden = document.getElementById('burden')
+const career = document.getElementById('career')
+
+const getFeedbackData = async (url) =>
+{
+    const response = await fetch(url)
+    const result = await response.json()
+    return result
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+   const processData = async () => {
+    const a = await getFeedbackData(API_URL)
+    const id = a.length + 1
+    const feedback = {
+        id: `${id}`,
+        name: `${userName.value}`,
+        email: `${email.value}`,
+        join: `${join.value}`,
+        travel: `${travel.value}`,
+        words: `${words.value}`,
+        most: `${most.value}`,
+        burden: `${burden.value}`,
+        career: `${career.value}`
+    }
+    postData(API_URL, feedback)
+   }
+    
+//     fetch("https://script.google.com/macros/s/AKfycbzBE-Yzh9ISu2H0vWwBD_jHoSXXbjsTVQDzq4YBq7cCgc8Su_EsXa7wy_LoNWeyElRu/exec", {
+//         method: "POST",
+//         body: data
+//     }).then(res => res.text).then(data => {form.reset(); alert('Data Submitted Successfully')}
+//    );
+    processData()
+})
+
+
+
+const postData = async(url, feedback) =>
+{
+    try
+    {
+        const result = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(feedback)
+        })
+        console.log(result)
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+}
--- index.js
+++ index.js
@@ -43,20 +43,20 @@ var form = document.getElementById("myForm"),
    newUserBtn = document.querySelector('.newUser')
 
 
-  let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : []
+let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : []
 
-  let isEdit = false, editId
-  showInfo()
+let isEdit = false
+showInfo()
 
-  newUserBtn.addEventListener('click' ,()=> {
+newUserBtn.addEventListener('click' ,()=> {
     submitBtn.innerText = 'Submit' ,
     modalTitle.innerText = "Fill the Form"
     isEdit = false
     imgInput.src = "Images/user-1.jpg"
     form.reset()
-  })
+})
 
-  file.onchange = function(){
+file.onchange = function(){
     if(file.files[0].size < 1000000){
         var fileReader = new FileReader();
 
@@ -69,33 +69,77 @@ var form = document.getElementById("myForm"),
     else {
         alert("This file is too large !")
     }
-  }
+}
 
-  function showInfo(){
+
+function showInfo(){
     document.querySelectorAll('.employeeDetails').forEach(info => info.remove())
     getData.forEach((element, index) => {
-        let createElement = `<tr class="employeeDetails">
-         <td>${index+1}</td>
-         <td><img src="${element.picture}" alt="" width="50" height="50"></td>
-         <td>${element.employeeName}</td>
-         <td>${element.employeeEmail}</td>
-         <td>${element.employeeMobile}</td>
-         <td>${element.startDate}</td>
+            const handleFeedback = (mail) => {
+            const mailSplit = mail
+            
+            const feedbackCheck = async (mailSplit) => {
+                const feedbackResponse = await fetch("http://localhost:3000/feedback")
+                const feedbackJson = await feedbackResponse.json()
+                
+                for(let feedback of feedbackJson)
+                {
+                    if(feedback.email === mailSplit)
+                    {
+                        fstatus = true
+                        break
+                    }
+                    else
+                    {
+                        fstatus = false
+                    }
+                }
 
-         <td>
-         <button class="btn btn-success" onclick="readInfo('${element.picture}','${element.employeeName}','${element.employeeEmail}', '${element.employeeMobile}','${element.startDate}' )" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
-         <button class="btn btn-primary" onclick="editInfo(${index}, '${element.picture}','${element.employeeName}','${element.employeeEmail}', '${element.employeeMobile}','${element.startDate}' )" data-bs-toggle="modal" data-bs-target="#userForm"><i class="bi bi-pencil-square"></i></button>
-         <button class="btn btn-danger" onclick="deleteInfo(${index})"><i class="bi bi-trash"></i></button>
-     
-         </td>
+                if(fstatus)
+                {
+                    let createElement = `<tr class="employeeDetails">
+                        <td>${index+1}</td>
+                        <td><img src="${element.picture}" alt="" width="50" height="50"></td>
+                        <td>${element.employeeName}</td>
+                        <td>${element.employeeEmail}</td>
+                        <td>${element.employeeMobile}</td>
+                        <td>${element.startDate}</td>
 
+                        <td>
+                            <button class="btn btn-success" onclick="readInfo('${element.picture}','${element.employeeName}','${element.employeeEmail}', '${element.employeeMobile}','${element.startDate}' )" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
+                            <button class="btn btn-primary" onclick="editInfo(${index}, ${element.id}, '${element.picture}','${element.employeeName}','${element.employeeEmail}', '${element.employeeMobile}','${element.startDate}' )" data-bs-toggle="modal" data-bs-target="#userForm"><i class="bi bi-pencil-square"></i></button>
+                            <button class="btn btn-danger" onclick="deleteInfo(${index},${element.id})"><i class="bi bi-trash"></i></button>
+                        </td>
+                        <td><p class = 'feedbackstatus'>Submitted</p></td>
+                    </tr>`
+                    userInfo.innerHTML += createElement
+                }
+                else
+                {
+                    let createElement = `<tr class="employeeDetails">
+                        <td>${index+1}</td>
+                        <td><img src="${element.picture}" alt="" width="50" height="50"></td>
+                        <td>${element.employeeName}</td>
+                        <td>${element.employeeEmail}</td>
+                        <td>${element.employeeMobile}</td>
+                        <td>${element.startDate}</td>
 
-       </tr>`
-
-       userInfo.innerHTML += createElement
+                        <td>
+                            <button class="btn btn-success" onclick="readInfo('${element.picture}','${element.employeeName}','${element.employeeEmail}', '${element.employeeMobile}','${element.startDate}' )" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
+                            <button class="btn btn-primary" onclick="editInfo(${index}, ${element.id}, '${element.picture}','${element.employeeName}','${element.employeeEmail}', '${element.employeeMobile}','${element.startDate}' )" data-bs-toggle="modal" data-bs-target="#userForm"><i class="bi bi-pencil-square"></i></button>
+                            <button class="btn btn-danger" onclick="deleteInfo(${index},${element.id})"><i class="bi bi-trash"></i></button>
+                        </td>
+                        <td><p class = 'feedbackstatus'>Pending</p></td>
+                    </tr>`
+                    userInfo.innerHTML += createElement
+                }
+            }
+        
+            feedbackCheck(mailSplit)
+        }
+        handleFeedback(element.employeeEmail)
     })
-  }
-showInfo()
+}
 
 
 function  readInfo(pic,name,email,phone,sDate){
@@ -107,7 +151,9 @@ function  readInfo(pic,name,email,phone,sDate){
 
 }
 
-function editInfo(index,pic,name,Email,Phone,Sdate){
+function editInfo(index,id,pic,name,Email,Phone,Sdate){
+
+    jsonEditId = id
     isEdit = true
     editId = index
     imgInput.src = pic
@@ -118,64 +164,122 @@ function editInfo(index,pic,name,Email,Phone,Sdate){
 
     submitBtn.innerText = "Update"
     modalTitle.innerText = "Update The Form"
+
 }
-
-
-
-  function deleteInfo(index){
+  function deleteInfo(index,id){
     if(confirm("Are you sure want to delete?")){
         getData.splice(index, 1)
         localStorage.setItem("userProfile", JSON.stringify(getData))
         showInfo()
+        deleteUser(id)
     }
   }
 
-  form.addEventListener('submit', (e)=> {
+submitBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    const information = {
-        picture:  imgInput.src == undefined ? "Images/user-1.jpg" : imgInput.src,
-        employeeName: userName.value,
-        employeeEmail: email.value,
-        employeeMobile: phone.value,
-        startDate: sDate.value
+    if(submitBtn.innerHTML === 'Submit'){
+        const userList = JSON.parse(localStorage.getItem("userProfile"))
 
-
+        const information = {
+            id: userList.length ? userList[userList.length - 1].id + 1 : 1,
+            picture:  imgInput.src == undefined ? "Images/user-1.jpg" : imgInput.src,
+            employeeName: userName.value,
+            employeeEmail: email.value,
+            employeeMobile: phone.value,
+            startDate: sDate.value,
+        }
+        console.log(information)
+        getData.push(information)
+        handleSubmit()
     }
-    if(!isEdit){
-             getData.push(information)
-    }
-    else{
-        isEdit = false
+    else if(submitBtn.innerHTML === 'Update'){
+        const information = {
+            id: jsonEditId,
+            picture:  imgInput.src == undefined ? "Images/user-1.jpg" : imgInput.src,
+            employeeName: userName.value,
+            employeeEmail: email.value,
+            employeeMobile: phone.value,
+            startDate: sDate.value
+        }
         getData[editId] = information
+        handleEditUser(jsonEditId,information)
     }
     localStorage.setItem('userProfile', JSON.stringify(getData))
       
     
      submitBtn.innerText = "Submit"
      modalTitle.innerHTML = "Fill The Form"
-
-     showInfo()
      form.reset()
 
      imgInput.src = "Images/user-1.jpg"
      modal.style.display = "none"
      document.querySelector(".modal-backdrop").remove()
+
 })
 
 
+// JSON Coding
 
+const API_URL = "http://localhost:5501/users"; // Local server JSON API
 
+function handleSubmit() {
+    const userList = JSON.parse(localStorage.getItem("userProfile"))
+    const id = userList.length ? userList[userList.length - 1].id + 1 : "1"
+    const user = {
+        id : `${id}`,
+        picture:  imgInput.src == undefined ? "Images/user-1.jpg" : imgInput.src,
+        employeeName: `${userName.value}`,
+        employeeEmail: `${email.value}`,
+        employeeMobile: `${phone.value}`,
+        startDate: `${sDate.value}`
+    }
+   postData(API_URL, user)
+}
 
+const postData = async (API_URL, user) => {
+    console.log(JSON.stringify(user))
+    try
+    {
+        const response = await fetch(API_URL, {
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(user)
+        })
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+}
 
+const deleteUser = async (id) => {
+    try
+    {
+        const result = await fetch(`${API_URL}/${id}`, {
+            method : "DELETE",
+        })
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
+const handleEditUser = async (id, editUser) => {
+    try
+    {
+        const result = await fetch(`${API_URL}/${id}`, {
+            method : "PUT",
+            headers : {"Content-Type" : "application/json"},
+            body : JSON.stringify(editUser)
+        })
+        console.log(result)
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+}
 
